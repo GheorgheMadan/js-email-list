7fbaa24cdd0a164852a4cde8441accf20c1abab0
Gheorghe <madangheorghe.30@gmail.com>
2025-01-22 15:39:42 +0100

    add axios

--- js/script.js
+++ js/script.js
@@ -8,17 +8,35 @@
 const output = document.getElementById('output')
 console.log(output)
 
-// const emailArr = []
-// creo un ciclo che mi stampi automaticamente 10 numeri casuali per provare il ciclo
+// setto l'endpoint
+const endPoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+// creo un ciclo che mi stampi automaticamente 10 mail
+const numArr = []
+let items = ''
 for(let i = 1; i <= 10; i++){
-    console.log(generaNumeroRandomRange(1, 10));
-    
+    // console.log(endPoint);
+    // const numRandom = generaNumeroRandomRange(1, 10)
+    // numArr.push(numRandom)
+    // items += `<li>${numRandom}</li>`
+     axios.get(endPoint)
+        .then(responseEmail =>{
+            const email = responseEmail.data.response;
+            console.log(email)
+            output.innerHTML += `<li>${email}</li>`
+        })
+        .catch(error =>{
+            // codice da eseguire in caso di errore 
+            console.error(error);
+        })
 }
 
 
-// FUNZIONE CHE GENERA UN NUMERO DA ... A ... I NUMERI LI SCELGO IO
-console.log(generaNumeroRandomRange(100, 50))
 
+
+
+
+// FUNZIONE CHE GENERA UN NUMERO DA ... A ...
 function generaNumeroRandomRange(max, min){
     const numeroGenerato = Math.floor(Math.random() * (max - min + 1)) + min;
     
